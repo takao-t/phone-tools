1b84f4c7cf1e00c35da653fe64f0270e9c8720c8
Takao Takahashi <tsq@gapj.net>
2020-11-10 15:39:14 +0900

    separete config files

--- gs/README.md
+++ gs/README.md
@@ -1,1 +1,12 @@
-テンプレートはGXP213で8パークキー
+GXP2135プロビジョン
+
+cfg.xml 共通設定ファイル
+↓
+cfggxp2135.xmk キー情報設定ファイル
+↓
+cfg{MACaddress].xml 各端末情報設定ファイル(SIPアカウント等)
+
+『すべての利用可能な設定ファイルのダウンロードと処理』をYESに設定すること
+
+ cfggxp2135.xml - パークx8 設定
+ cfggxp2135_keysample.xml - ラインx4、パークx4
--- gs/mkcfg.sh
+++ gs/mkcfg.sh
@@ -2,11 +2,13 @@
 #
 # Grandstream GXP-2135 プロビジョン用ファイル生成スクリプト
 #
-# どのパラメータがGSのPコードに対応するかはテンプレートを参照のこと
-#
+
 
 # テンプレートファイル
-TEMPLATE="./gxp2135_templ.xml"
+# 共通テンプレート
+CTEMPLATE="./common_templ.xml"
+# デバイス毎テンプレート
+DTEMPLATE="./dev_templ.xml"
 
 # デバイスリスト
 LIST="./gs_phones.txt"
@@ -20,9 +22,12 @@ PREFIX="cfg"
 SUFFIX="xml"
 
 # プロビジョンサーバとパスの指定
-# 電話帳ダウンロードも同じパスを使用する
-PROVS="192.168.1.1/gs"
+PROVS="192.168.254.175/gs"
 
+# 共通プロビジョンファイル生成
+cat $CTEMPLATE |  sed s/##DISPNAME##/$DISPNAME/  | sed s/##MACADDR##/$MACADDR/ |  sed s^##PROVSERVER##^$PROVS^ | sed s/##RINGTONE##/$RINGTONE/ > $PREFIX.$SUFFIX
+
+# 個別プロビジョンファイル生成
 while read LINE
 do
     if echo $LINE | grep '^#' > /dev/null 2>&1;
@@ -30,13 +35,14 @@ do
         continue
     fi
     # echo $LINE
-    MACADDR=`echo $LINE | cut -f1,1 -d','`
+    MACADDR=`echo $LINE | cut -f1,1 -d',' | tr [:upper:] [:lower:]`
+    UMACADDR=`echo $MACADDR | tr [:lower:] [:upper:]`
     DISPNAME=`echo $LINE | cut -f2,2 -d','`
     SIPSERVER=`echo $LINE | cut -f3,3 -d','`
     USERNAME=`echo $LINE | cut -f4,4 -d','`
     PASSWORD=`echo $LINE | cut -f5,5 -d','`
-    TARGET=`echo $MACADDR | tr [A-Z] [a-z]`
+    TARGET=$MACADDR
 
-    cat $TEMPLATE | sed s/##SIPSERVER##/$SIPSERVER/  | sed s/##SIPUSERNAME##/$USERNAME/  | sed s/##SIPPASSWORD##/$PASSWORD/  | sed s/##DISPNAME##/$DISPNAME/  | sed s/##MACADDR##/$MACADDR/ | sed s/##RINGTONE##/$RINGTONE/ | sed s^##PROVSERVER##^$PROVS^ > $PREFIX$TARGET.$SUFFIX
+    cat $DTEMPLATE | sed s/##SIPSERVER##/$SIPSERVER/  | sed s/##SIPUSERNAME##/$USERNAME/  | sed s/##SIPPASSWORD##/$PASSWORD/  | sed s/##DISPNAME##/$DISPNAME/  | sed s/##MACADDR##/$MACADDR/ | sed s/##UMACADDR##/$UMACADDR/ > $PREFIX$TARGET.$SUFFIX
     echo "$TARGET - done"
 done < $LIST
